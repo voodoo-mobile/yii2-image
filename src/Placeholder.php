ae0657f5cfa64a7f275423b95a9839b68737eb88
Alexander Kryshtalev <alex@voodoo-mobile.com>
2016-02-10 02:57:46 +0600

    [+] Migrated code from yii2vm

---
+++ src/Placeholder.php
@@ -0,0 +1,40 @@
+<?php
+namespace vm\image;
+
+use yii\base\Component;
+
+/**
+ * Class Placeholder
+ * @package vm\image
+ *
+ *          Provides the way to return a placeholder.
+ *          Use:
+ *              set up value as function($width, $height) {
+ *                                  return 'http://imagegenerator.com/width/height';
+ *                              }
+ *
+ *          This class is used in ImageBehavior. In most cases no need to use it directly
+ */
+class Placeholder extends Component
+{
+    /**
+     * Default placeholder size
+     */
+    const DEFAULT_SIZE = 320;
+
+    /**
+     * @var
+     */
+    public $value;
+
+    /**
+     * @param $width
+     * @param $height
+     *
+     * @return mixed
+     */
+    public function getImage($width, $height)
+    {
+        return call_user_func($this->value, $width, $height);
+    }
+}
